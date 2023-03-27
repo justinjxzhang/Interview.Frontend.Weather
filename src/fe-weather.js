ea39f69ac16dbbfed217c2a004f7c43bf70f5d0d
Justin Zhang <justin.j.x.zhang@gmail.com>
2023-03-27 15:43:21 +0100

    init

---
+++ src/fe-weather.js
@@ -0,0 +1,18 @@
+import { LitElement, html, css } from 'lit';
+
+class FeWeather extends LitElement {
+
+  static styles = css`
+  `;
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+    `;
+  }
+}
+
+customElements.define('fe-weather', FeWeather);
